24e68549a334031fe5b303a1aa12965ab2ce6394
Francis Tan <tan.francis.e@gmail.com>
2015-01-12 21:41:25 -0600

    Add Twitter live feed

--- mapfeed/views.py
+++ mapfeed/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.conf import settings
 from mapfeed.models import MapCity
+import twitter
 
 def index(request):
     city_list = MapCity.objects.all()
     return render_to_response('mapfeed/index.html', {'city_list': city_list})
 
 def details(request, city_id):
-    c = get_object_or_404(MapCity, pk=city_id)
-    return render_to_response('mapfeed/details.html', {'city': c})
+    city = get_object_or_404(MapCity, pk=city_id)
+    
+    api = twitter.Api(consumer_key=settings.TWITTER_CONSUMER_KEY, consumer_secret=settings.TWITTER_CONSUMER_SECRET, access_token_key=settings.TWITTER_TOKEN, access_token_secret=settings.TWITTER_TOKEN_SECRET)
+    statuses = api.GetUserTimeline(screen_name='MetroRailInfo')
+
+    return render_to_response('mapfeed/details.html', {'city': city, 'statuses': statuses})
