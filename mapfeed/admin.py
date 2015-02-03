5b6a920da4f634f7de2480660b359eb3d1f130d0
Francis Tan <tan.francis.e@gmail.com>
2015-02-02 23:40:45 -0600

    add list columns to admin site

--- mapfeed/admin.py
+++ mapfeed/admin.py
@@ -1,8 +1,23 @@
 from mapfeed.models import MapPath, MapCity, MapLine, MapStation, Incident
 from django.contrib import admin
 
-admin.site.register(MapCity)
-admin.site.register(MapLine)
-admin.site.register(MapPath)
-admin.site.register(MapStation)
-admin.site.register(Incident)
+class MapCityAdmin(admin.ModelAdmin):
+    list_display = ('city_name', 'rail_name')
+
+class MapLineAdmin(admin.ModelAdmin):
+    list_display = ('line_name', 'line_code')
+
+class MapPathAdmin(admin.ModelAdmin):
+    pass
+
+class MapStationAdmin(admin.ModelAdmin):
+    list_display = ('station_name', 'station_code')
+
+class IncidentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(MapCity, MapCityAdmin)
+admin.site.register(MapLine, MapLineAdmin)
+admin.site.register(MapPath, MapPathAdmin)
+admin.site.register(MapStation, MapStationAdmin)
+admin.site.register(Incident, IncidentAdmin)
