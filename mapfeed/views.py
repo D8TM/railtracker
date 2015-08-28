9c2f47b9ff1b4cc524d2ea62f91f24291a5e6b75
Francis Tan <tan.francis.e@gmail.com>
2015-08-27 20:25:54 -0500

    display lines. update line drawing algorithm.

--- mapfeed/views.py
+++ mapfeed/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.conf import settings
-from mapfeed.models import MapCity, MapLine, MapStation, MapPath
-import twitter
+from mapfeed.models import MapCity, MapLine, MapStation, MapPath, Incident
+import twitter, datetime
 
 def index(request):
     city_list = MapCity.objects.all()
@@ -14,7 +14,8 @@ def details(request, city_id):
     line_ids = MapLine.objects.values('id').filter(map=city_id)
     stations = MapStation.objects.filter(lines__id__in=line_ids).distinct().order_by('station_name')
     paths = MapPath.objects.filter(line__id__in=line_ids)
-    
+    incidents = Incident.objects.filter(incident_date__gte=datetime.date.today())
+
     api = twitter.Api(
             consumer_key=settings.TWITTER_CONSUMER_KEY,
             consumer_secret=settings.TWITTER_CONSUMER_SECRET,
@@ -28,17 +29,24 @@ def details(request, city_id):
         'statuses': statuses,
         'lines': lines,
         'stations': stations,
-        'paths': paths
+        'paths': paths,
+        'incidents':incidents,
     })
 
 def line(request, city_id, line_id):
     city = get_object_or_404(MapCity, pk=city_id)
     line = get_object_or_404(MapLine, pk=line_id)
+    stations = MapStation.objects.filter(lines__id__in=line).order_by('station_name')
+    paths = MapPath.objects.filter(line__id__in=line_ids)
+    incidents = Incident.objects.filter(incident_date__gte=datetime.date.today())
 
-    return render_to_response('mapfeed/line.html',
-            {'city': city,
-                'line': line}
-    )
+    return render_to_response('mapfeed/line.html', {
+        'city': city,
+        'line': line,
+        'stations': stations,
+        'paths': paths,
+        'incidents': incidents,
+    })
 
 def about(request):
     return render_to_response('mapfeed/about.html')
--- sitetags/templatetags/graphic_tags.py
+++ sitetags/templatetags/graphic_tags.py
@@ -16,6 +16,18 @@ def draw_line(line):
     }
 register.inclusion_tag('mapfeed/path_draw.html')(draw_line)
 
+#determine line order for drawing
+@register.filter
+def determine_line_order(line, station):
+    order = 1
+    paths = MapPath.objects.filter(station__id=station.id).order_by('line')
+    #iterates through each path segment, compares to line given
+    for p in paths:
+        if line.id == p.line.id:
+            return order
+        order = order + 1
+    return 0
+
 #returns the next station given a specific key
 @register.filter
 def next_station_x(list, key):
@@ -32,3 +44,19 @@ def next_station_y(list, key):
     except Exception as e:
         station = list[key]
     return station.y_coord
+
+@register.filter
+def next_station_num_lines(list, key):
+    try:
+        station = list[key+1]
+    except Exception as e:
+        station = list[key]
+    return station.lines.count
+
+@register.filter
+def next_station_name(list, key):
+    try:
+        station = list[key+1]
+    except Exception as e:
+        station = list[key]
+    return station.station_name
